17401b6f5eb5ac16ac20c5b0ded9c74b956d215a
Luiz Felipe <luizfelipe.consultoria20@gmail.com>
2024-03-07 05:11:15 -0300

    update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,8 +3,33 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <img src="/img/fundo.png" alt="" />
-      <img src="/img/logo.png" alt="" />
+      <section>
+        <ul>
+          <li>
+            <a href="facebook.com" target="_blank">
+              <img src="/img/fb.png" alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="twitter.com" target="_blank">
+              <img src="/img/tw.png" alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="instagram.com" target="_blank">
+              <img src="/img/ig.png" alt="" />
+            </a>
+          </li>
+        </ul>
+      </section>
+
+      <section>
+        <img src="/img/logo.png" alt="" />
+      </section>
+
+      <section>
+        <p>Desenvolvido por Alura.</p>
+      </section>
     </footer>
   );
 };
